f48ea73f2b842f2d6f94f6b3a6d081112aca3c43
BATMINJI <105769332+batminji@users.noreply.github.com>
2023-10-18 18:28:42 +0900

    Drill_10_2022180014_박민지

---
+++ Lecture12_Game_World/game_world.py
@@ -0,0 +1,27 @@
+from pico2d import *
+
+objects = [[], []]
+
+def add_object(o, depth = 0):
+    objects[depth].append(o)
+
+
+def add_objects(ol, depth = 0):
+    objects[depth] += ol
+
+def update():
+    for layer in objects:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in objects:
+        for o in layer:
+            o.draw()
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('존재하지 않은 객체를 지우려고 합니다.')
